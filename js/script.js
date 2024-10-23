b502a32da9e5a55f5333c3db050feaf8e18d5b05
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-10-23 16:12:16 +0200

    add input & button html

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("js Biglietto Treno")
+
+// Scrivere un programma che chieda all’utente (prompt)
+// - Il numero di chilometri da percorrere -> Primo INPUT
+// - Età del passeggero -> Secondo INPUT
+
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km) -> km * 0,21
+// - va applicato uno sconto del 20% per i minorenni -> sale=priceTicket * 20 / 100
+// - va applicato uno sconto del 40% per gli over 65 -> sale=priceTicket * 40 / 100
